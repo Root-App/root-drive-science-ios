dc517e83969512f72df9fb6f0f66610fd88de12a
Austin Emmons <austin.emmons@joinroot.com>
2020-02-03 11:31:18 -0600

    Refactors handling of TelematicsManager such that ViewController is passed in as delegate.
    This removes the need for NSNotifications to be published when receiving
    delegate callbacks.
    Also removes TelematicsManager singleton and makes initialization
    process explicit in ViewController
    
    These changes will hopefully make the initialization flow and delegate
    method callbacks easier to follow for client developers

---
+++ root-drive-science-ios/ViewController+TripTrackerDelegates.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController+TripTrackerDelegates.swift
+//  root-drive-science-ios
+//
+//  Created by Austin Emmons on 2/3/20.
+//  Copyright © 2020 Root. All rights reserved.
+//
+
+import RootTripTracker
+
+extension ViewController: TripTrackerClientDelegate {
+    func didStartTrip(_ tripId: String) {
+        self.appendNotificationText("Trip started with id \(tripId)")
+    }
+
+    func didEndTrip(_ tripId: String) {
+        self.appendNotificationText("Trip ended with id \(tripId)")
+    }
+}
+
+extension ViewController: TripTrackerDriveScienceManagerDelegate {
+    func didReceiveDriverId(_ driverId: String) {
+        // save our own reference to activeDriverId
+        telematicsManager.activeDriverId = driverId
+        self.appendNotificationText("Created driver: \(driverId)")
+    }
+
+    func didNotReceiveDriverId(_ errorMessage: String) {
+        telematicsManager.activeDriverId = nil
+        self.appendNotificationText("Unable to create driver: \(errorMessage)")
+    }
+
+    func activationDidSucceed(_ manager: TripTrackerDriveScienceManager) {
+        self.appendNotificationText("Activated successfully.")
+    }
+
+    func activationDidFail(_ manager: TripTrackerDriveScienceManager, errorMessage: String) {
+        self.appendNotificationText("Unable to activate: \(errorMessage)")
+    }
+}
+
+extension ViewController: TripTrackerDelegate {
+    func tripTracker(
+        _ tripTracker: TripTracker,
+        didTrackAnalyticsEvent eventName: String,
+        withProperties properties: [String: Any])
+    {
+        self.appendNotificationText(eventName)
+    }
+
+    func tripTracker(
+        _ tripTracker: TripTracker,
+        didFailWithError error: TripTrackerError)
+    {
+        self.appendNotificationText("Trip Tracker error.")
+    }
+}
--- root-drive-science-ios/ViewController.swift
+++ root-drive-science-ios/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 import RootTripTracker
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController {
+
+    var telematicsManager: TelematicsManager!
     
     @IBOutlet var trackingLabel: UILabel!
     @IBOutlet var notificationField: UITextView!
@@ -19,28 +21,47 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var phoneTextField: UITextField!
     
     var environmentData: [String] = [String]()
+
+    lazy var clientId: String = {
+        return Bundle.main.object(forInfoDictionaryKey: "RootClientId") as! String
+    }()
+
+    private lazy var dateFormat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone.current
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        return formatter
+    }()
+
+    var timestamp: String {
+        return dateFormat.string(from: Date())
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupTelematics()
+
         trackingLabel.text = "Waiting to start..."
         notificationField.text = ""
-
-        observe(.didTrackAnalyticsEvent, selector: #selector(onDidTrackAnalyticsEvent(_:)))
-        observe(.didFailWithError, selector: #selector(onDidFailWithError(_:)))
-        observe(.didStartTrip, selector: #selector(onDidStartTrip(_:)))
-        observe(.didEndTrip, selector: #selector(onDidEndTrip(_:)))
-
-        observe(.didReceiveDriverId, selector: #selector(didReceiveDriverId(_:)))
-        observe(.didNotReceiveDriverId, selector: #selector(didNotReceiveDriverId(_:)))
-
-        observe(.activationDidSucceed, selector: #selector(activationDidSucceed(_:)))
-        observe(.activationDidFail, selector: #selector(activationDidFail(_:)))
     }
+
+    private func setupTelematics() {
+        let driveScienceManager = TripTrackerDriveScienceManager(
+            clientId: clientId,
+            environment: .staging,
+            delegate: self,
+            clientDelegate: self
+        )
+
+        driveScienceManager.tripTracker.delegate = self
+        telematicsManager = TelematicsManager(driveScienceManager: driveScienceManager)
+    }
+
+    // MARK: UI Helpers
     
     func appendNotificationText(_ text: String) {
         DispatchQueue.main.async {
-            self.notificationField.text = "[\(self.timestamp())]:" + text + "\n" + (self.notificationField.text ?? "")
+            self.notificationField.text = "[\(self.timestamp)]:" + text + "\n" + (self.notificationField.text ?? "")
         }
     }
     
@@ -50,8 +71,10 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
     }
 
+    // MARK: UI Actions
+
     @IBAction func createDriver(_ sender: UIButton) {
-        TelematicsManager.sharedManager.createDriver(
+        telematicsManager.createDriver(
             driverId: driverIdTextField.text,
             email: emailTextField.text,
             phone: emailTextField.text
@@ -60,39 +83,19 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     @IBAction func startTracking(_ sender: UIButton) {
         setTrackingText("Waiting...")
-        TelematicsManager.sharedManager.start()
+        if telematicsManager.hasActiveDriver {
+            _ = telematicsManager.start()
+        } else {
+            self.appendNotificationText("No active driver.")
+        }
     }
     
     @IBAction func stopTracking(_ sender: UIButton) {
-        TelematicsManager.sharedManager.stopTracker()
+        telematicsManager.stop()
         setTrackingText("Tracking Stopped")
         self.appendNotificationText("Tracking Stopped")
     }
-    
-    @IBAction func resetTokens() {
-        setTrackingText("Resetting...")
-        TelematicsManager.sharedManager.removeAllTokens()
-    }
-    
-    @objc func didReceiveDriverId(_ notification: Notification) {
-        let driverId = notification.userInfo?["driver_id"] as! String
-        self.appendNotificationText("Created driver: \(driverId)")
-    }
 
-    @objc func didNotReceiveDriverId(_ notification: Notification) {
-        let message = notification.userInfo?["message"] ?? ""
-        self.appendNotificationText("Unable to create driver: \(message)")
-    }
-
-    @objc func activationDidSucceed(_ notification: Notification) {
-        self.appendNotificationText("Activated successfully.")
-    }
-
-    @objc func activationDidFail(_ notification: Notification) {
-        let message = notification.userInfo?["message"] ?? ""
-        self.appendNotificationText("Unable to activate: \(message)")
-    }
-    
     @objc func onDidTrackAnalyticsEvent(_ notification: Notification) {
         let eventName = notification.userInfo!["eventName"] as! String
         self.appendNotificationText(eventName)
@@ -102,51 +105,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         self.appendNotificationText("Error")
     }
     
-    @objc func onDidStartTrip(_ notification: Notification) {
-        let tripId = notification.userInfo!["tripId"] as! String
-        self.appendNotificationText("Trip started with id \(tripId)")
-    }
-    
-    @objc func onDidEndTrip(_ notification: Notification) {
-        let tripId = notification.userInfo!["tripId"] as! String
-        self.appendNotificationText("Trip ended with id \(tripId)")
-    }
-    
-    func timestamp() -> String {
-        let now = Date()
-        let formatter = DateFormatter()
-        formatter.timeZone = TimeZone.current
-        formatter.dateFormat = "yyyy-MM-dd HH:mm"
-        return formatter.string(from: now)
-    }
-    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return environmentData.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return environmentData[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let selection = environmentData[row]
-        let environment: EnvironmentType = (selection == "Local") ? .local : .staging
-        TelematicsManager.sharedManager.environment = environment
-    }
-
-    func observe(_ name: Notification.Name, selector: Selector) {
-        NotificationCenter.default.addObserver(
-        self,
-        selector: selector,
-        name: name, object: nil)
-    }
-
-    deinit {
-        NotificationCenter.default.removeObserver(self)
     }
 }
 
